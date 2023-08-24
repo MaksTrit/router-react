fb95857b3787f110135628d54f936a8a5366459c
Maksym Triteinikov <110408944+MaksTrit@users.noreply.github.com>
2023-08-24 17:13:11 +0300

    Initial commit

---
+++ src/components/Layout.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Layout = styled.main`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  gap: 20px;
+  padding: 16px;
+`;
